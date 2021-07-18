557b89ad78485b1ada22e7ca5c81b2029d7be2da
Harsha <kumavatharsha20@gmail.com>
2021-07-18 10:47:37 +0530

    add addition_2_num.py

---
+++ Addition_2_num.py
@@ -0,0 +1,14 @@
+#addition of 2 numbers
+
+"""using input() function to take input from user
+input() function stores values in string data type
+for example user enter 2 it will stored as "2" in string format
+print() function is used to print the contents mentioned within it
+"+" sign is used to concatenate the numbers but as numbers are in string format we convert them
+in integer format using int()
+"""
+n1 = input("enter first number : ")
+n2 = input("enter second number : ")
+print("First number : ",n1)
+print("Second number : ",n2)
+print("Addition of two numbers is : ",int(n1) + int(n2))
