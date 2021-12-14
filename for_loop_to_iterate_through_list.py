64f1f4fcd784dc301c615c74053142f6b6621afe
Harsha <87359806+20Harsha@users.noreply.github.com>
2021-12-14 18:40:57 +0530

    code updated

--- for_loop_to_iterate_through_list.py
+++ for_loop_to_iterate_through_list.py
@@ -2,10 +2,12 @@
 #We are printing those numbers which are of integer datatype and greater than equal to 6
 #If strings are there then it will display that these are strings.
 list1 = [2,4,3,1,6,3,7,9,10,"hello",23,45,3,"mona",67,34]
+l=[]
 for x in list1:
     if type(x)== int:
         if x >=6 :
-            print(x)
+            l.append(x)
+            print('numbers greater than equal to 6',l)
     else:
         print("These are strings : ",x)
 
